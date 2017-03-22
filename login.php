afa15dcc5ca930234222419ae79f401b9ffdbd2b
riddhi171997 <patelriddhi.parimalkumar2015@vit.ac.in>
2017-03-22 23:09:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <?php 
-if($_POST[password]=="12345" && ($_POST[regno]=="15BIT0246" || $_POST[regno]=="15bit0246"))
+if($_POST[password]=="12345" && ($_POST[regno]=="15BIT0258" || $_POST[regno]=="15bit0258"))
 {
 	header("location:successful.php");
 	$name=$_POST[name];
